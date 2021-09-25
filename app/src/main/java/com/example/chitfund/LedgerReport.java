67b3fcf3c0c76cebdb613e960fee9bb894295838
MRinshad <rinshadmorayur09@gmail.com>
2021-09-25 20:32:53 +0530

    added new menu on homepage and linked every homepage button

---
+++ app/src/main/java/com/example/chitfund/LedgerReport.java
@@ -0,0 +1,14 @@
+package com.example.chitfund;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LedgerReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ledger_report);
+    }
+}
--- app/src/main/java/com/example/chitfund/MainActivity.java
+++ app/src/main/java/com/example/chitfund/MainActivity.java
@@ -2,21 +2,51 @@ package com.example.chitfund;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    androidx.appcompat.widget.Toolbar myToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        ActionBar actionBar = getActionBar();
+//        actionBar.setSubtitle("mytest");
+//
     }
     public void addMembers(View v){
         Intent i = new Intent(this,AddMember.class);
         startActivity(i);
         finish();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.homepage_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu1:
+//                addSomething();
+                return true;
+            case R.id.menu2:
+//                startSettings();
+                return true;
+            case R.id.menu3:
+//                startSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
